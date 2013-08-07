dddc8a486c533916170d0834442af87b95d74069
Noah Isaacson <noah@nisaacson.com>
2013-08-07 09:07:55 -0600

    Bugfix: fix error message for missing outputPath. Bugfix: add check that repoPath exists on disk when validating data.

--- index.js
+++ index.js
@@ -1,57 +1,70 @@
+var fs = require('fs')
 var exec = require('child_process').exec
 
 module.exports = function(data, cb) {
 
-  var error = validateData(data)
-  if (error) {
-    return cb(error)
-  }
-  var repoPath = data.repoPath
-  var outputPath = data.outputPath
-  var command = buildCommand(data)
-  var opts = {
-    cwd: repoPath
-  }
-  exec(command, opts, function (err, stdout, stderr) {
-    var error
+  validateData(data, function(err) {
     if (err) {
-      error = new Error('failed to archive git commit')
-      error.source = err
-      error.stdout = stdout
-      error.stderr = stderr
-      return cb(error)
+      return cb(err)
     }
-    return cb(null, outputPath)
+    var repoPath = data.repoPath
+    var outputPath = data.outputPath
+    var command = buildCommand(data)
+    var opts = {
+      cwd: repoPath
+    }
+    exec(command, opts, function(err, stdout, stderr) {
+      var error
+      if (err) {
+        error = new Error('failed to archive git commit')
+        error.source = err
+        error.stdout = stdout
+        error.stderr = stderr
+        return cb(error)
+      }
+      return cb(null, outputPath)
+    })
   })
 }
 
-function buildCommand(data) {
-  var commit = data.commit
-  var outputPath = data.outputPath
-  var command = 'git archive -o ' + outputPath + ' ' + commit
-  // allow optional flags to be passed
-  if (data.flags) {
-    command += ' ' + data.flags.join(' ')
+  function buildCommand(data) {
+    var commit = data.commit
+    var outputPath = data.outputPath
+    var command = 'git archive -o ' + outputPath + ' ' + commit
+    // allow optional flags to be passed
+    if (data.flags) {
+      command += ' ' + data.flags.join(' ')
+    }
+    return command
   }
-  return command
-}
 
-function validateData(data) {
-  var error
-  if (!data.commit) {
-    error = new Error('failed to archive data. Missing key')
-    error.source = 'commit field missing in data object'
-    return error
+  function validateData(data, cb) {
+    var error
+
+    if (!data.commit) {
+      error = new Error('failed to archive data. Missing key')
+      error.source = 'commit field missing in data object'
+      return cb(error)
+    }
+
+    if (!data.outputPath) {
+      error = new Error('failed to archive data. Missing key')
+      error.source = 'outputPath field missing in data object'
+      return cb(error)
+    }
+
+    if (!data.repoPath) {
+      error = new Error('failed to archive data. Missing key')
+      error.source = 'repoPath field missing in data object'
+      return cb(error)
+    }
+
+    fs.exists(data.repoPath, function (exists) {
+      if (!exists) {
+        error = new Error('failed to archive data. Invalid data')
+        error.source = 'git repo does not exist on disk at given repoPath: ' + data.repoPath
+        return cb(error)
+      }
+      cb()
+    })
   }
-  if (!data.outputPath) {
-    error = new Error('failed to archive data. Missing key')
-    error.source = 'commit field missing in data object'
-    return error
-  }
-  if (!data.repoPath) {
-    error = new Error('failed to archive data. Missing key')
-    error.source = 'repoPath field missing in data object'
-    return error
-  }
-  return error
-}
--- test/git-archive-test.js
+++ test/git-archive-test.js
@@ -6,11 +6,69 @@ var fs = require('fs')
 var should = require('should')
 
 var gitArchive = require('../')
+
 describe('Git Archive', function() {
+  var data
+  var repoPath = path.join(__dirname, 'data/repos/apples_bare.git')
+  var outputPath = temp.path({suffix: 'apples.tar.gz'})
+
+  beforeEach(function() {
+    data = {
+      commit: '5de4caa20708131b1b29d3b6b3dea58e69d2c99c',
+      outputPath: outputPath,
+      repoPath: repoPath
+    }
+  })
+
+  it('should give error when outputPath is not specified', function (done) {
+    delete data.outputPath
+    gitArchive(data, function (err, reply) {
+      should.exist(err)
+      err.source.should.eql('outputPath field missing in data object')
+      should.not.exist(reply)
+      done()
+    })
+  })
+
+  it('should give error when commit is not specified', function (done) {
+    delete data.commit
+    gitArchive(data, function (err, reply) {
+      should.exist(err)
+      err.source.should.eql('commit field missing in data object')
+      should.not.exist(reply)
+      done()
+    })
+  })
+
+
+  it('should give error when repoPath is not specified', function (done) {
+    delete data.repoPath
+    gitArchive(data, function (err, reply) {
+      should.exist(err)
+      err.source.should.eql('repoPath field missing in data object')
+      should.not.exist(reply)
+      done()
+    })
+  })
+
+
+  it('should give error when repoPath does not exist on disk', function (done) {
+    var repoPath = '/fake/repo/path'
+    var desiredErrorSource = 'git repo does not exist on disk at given repoPath: ' + repoPath
+    assert.ok(!fs.existsSync(repoPath))
+    data.repoPath = repoPath
+    gitArchive(data, function (err, reply) {
+      should.exist(err)
+
+      err.source.should.eql(desiredErrorSource)
+      should.not.exist(reply)
+      done()
+    })
+  })
+
   it('should archive bare repo to tarball at given output path', function(done) {
-    var repoPath = path.join(__dirname, 'data/repos/apples_bare.git')
-    var outputPath = temp.path({suffix: 'apples.tar.gz'})
-    var data = {
+
+    data = {
       commit: '5de4caa20708131b1b29d3b6b3dea58e69d2c99c',
       outputPath: outputPath,
       repoPath: repoPath
