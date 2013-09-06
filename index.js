56433d80c664ec3e3e21195db028deb7dea0e084
Noah Isaacson <noah@nisaacson.com>
2013-09-05 19:40:48 -0600

    Use promises internally and refactor out validate.

--- index.js
+++ index.js
@@ -1,12 +1,15 @@
-var fs = require('fs')
+var errorAdapter = require('error-adapter')
+var q = require('q')
 var exec = require('child_process').exec
-
+var validate = require('./validate/archive-params')
 module.exports = function(data, cb) {
 
-  validateData(data, function(err) {
-    if (err) {
-      return cb(err)
-    }
+  return validate(data)
+    .then(archive)
+    .nodeify(cb)
+
+  function archive() {
+    var deferred = q.defer()
     var repoPath = data.repoPath
     var outputPath = data.outputPath
     var command = buildCommand(data)
@@ -16,55 +19,26 @@ module.exports = function(data, cb) {
     exec(command, opts, function(err, stdout, stderr) {
       var error
       if (err) {
-        error = new Error('failed to archive git commit')
-        error.source = err
+        error = errorAdapter.merge(err, {
+          namespace: 'git-archive'
+        })
         error.stdout = stdout
         error.stderr = stderr
-        return cb(error)
+        return deferred.reject(error)
       }
-      return cb(null, outputPath)
+      return deferred.resolve(outputPath)
     })
-  })
+    return deferred.promise
+  }
+};
+
+function buildCommand(data) {
+  var commit = data.commit
+  var outputPath = data.outputPath
+  var command = 'git archive -o ' + outputPath + ' ' + commit
+  // allow optional flags to be passed
+  if (data.flags) {
+    command += ' ' + data.flags.join(' ')
+  }
+  return command
 }
-
-  function buildCommand(data) {
-    var commit = data.commit
-    var outputPath = data.outputPath
-    var command = 'git archive -o ' + outputPath + ' ' + commit
-    // allow optional flags to be passed
-    if (data.flags) {
-      command += ' ' + data.flags.join(' ')
-    }
-    return command
-  }
-
-  function validateData(data, cb) {
-    var error
-
-    if (!data.commit) {
-      error = new Error('failed to archive data. Missing key')
-      error.source = 'commit field missing in data object'
-      return cb(error)
-    }
-
-    if (!data.outputPath) {
-      error = new Error('failed to archive data. Missing key')
-      error.source = 'outputPath field missing in data object'
-      return cb(error)
-    }
-
-    if (!data.repoPath) {
-      error = new Error('failed to archive data. Missing key')
-      error.source = 'repoPath field missing in data object'
-      return cb(error)
-    }
-
-    fs.exists(data.repoPath, function (exists) {
-      if (!exists) {
-        error = new Error('failed to archive data. Invalid data')
-        error.source = 'git repo does not exist on disk at given repoPath: ' + data.repoPath
-        return cb(error)
-      }
-      cb()
-    })
-  }
